7cd8bc75d7ee598d7394a57d39a8e46acb5aba46
NyanDesigns <nmyat@cpp.edu>
2023-03-05 12:27:15 -0800

    Link Updates

--- client/script.js
+++ client/script.js
@@ -90,7 +90,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codex-im0y.onrender.com/', {
+    const response = await fetch('https://gpt-analyzer.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
